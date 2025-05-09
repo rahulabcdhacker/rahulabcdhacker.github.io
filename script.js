7af4ec3bdbf0052ef3ceddbde69cf033472e4f84
rahulabcdhacker <kumarrahul17924@gmail.com>
2025-05-09 12:17:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
             // Cold Drink
             { id: 1, name: "Coca Cola (250ml)", price: 20, category: "cold-drink", image: "coca .jpg" },
             { id: 2, name: "Maaza (125ml)", price: 10, category: "cold-drink", image: "https://www.bigbasket.com/media/uploads/p/l/265725_18-maaza-juice-mango-refresh.jpg" },
-            { id: 3, name: "Litchi Drink (250ml)", price: 10, category: "cold-drink", image: "https://via.placeholder.com/200?text=Sprite" },
+            { id: 3, name: "Litchi Drink (250ml)", price: 10, category: "cold-drink", image: "https://dyyv8eonpc8dv.cloudfront.net/media/cache/f8/bc/f8bc9218424bfcd9c7e489e95172e820.jpg" },
             { id: 4, name: "Campa Orange (250ml)", price: 10, category: "cold-drink", image: "coca .jpg" },
             { id: 5, name: "Campa Green (250ml)", price: 10, category: "cold-drink", image: "coca .jpg" },
             { id: 6, name: "Campa Black (250ml) ", price: 10, category: "cold-drink", image: "https://via.placeholder.com/200?text=Pepsi" },
