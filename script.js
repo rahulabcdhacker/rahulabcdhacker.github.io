dd3f59234ce5840d123dacec2ab26f08c761d91a
rahul <kumarrahul17924@gmail.com>
2025-05-09 15:27:41 +0530

    allo

--- script.js
+++ script.js
@@ -266,7 +266,11 @@
             { id: 33, name: "Garam Masala", price: 40, category: "masale", image: "https://via.placeholder.com/200?text=Garam+Masala" },
             { id: 31, name: "Turmeric Powder", price: 30, category: "masale", image: "https://via.placeholder.com/200?text=Turmeric+Powder" },
             { id: 32, name: "Chili Powder", price: 25, category: "masale", image: "https://via.placeholder.com/200?text=Chili+Powder" },
-            { id: 33, name: "Garam Masala", price: 40, category: "masale", image: "https://via.placeholder.com/200?text=Garam+Masala" }
+            { id: 33, name: "Garam Masala", price: 40, category: "masale", image: "https://via.placeholder.com/200?text=Garam+Masala" },
+            //today offer
+              { id: 1, name: "Coca Cola (250ml)", price: 20, category: "today", image: "coca .jpg" },
+            { id: 2, name: "Maaza (125ml)", price: 10, category: "today", image: "https://www.bigbasket.com/media/uploads/p/l/265725_18-maaza-juice-mango-refresh.jpg" },
+            { id: 3, name: "Litchi Drink (250ml)", price: 10, category: "today", image: "https://dyyv8eonpc8dv.cloudfront.net/media/cache/f8/bc/f8bc9218424bfcd9c7e489e95172e820.jpg" },
         ];
 
         let cart = JSON.parse(localStorage.getItem('cart')) || [];
